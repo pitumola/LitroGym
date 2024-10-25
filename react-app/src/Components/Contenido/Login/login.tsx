f6e14ef849eb5b1ab6db89abb2c8c2710ac248b9
edumoro412 <edumoro2003@gmail.com>
2024-10-25 14:02:03 +0200

    Empezamos el login.css

--- react-app/src/Components/Contenido/Login/login.tsx
+++ react-app/src/Components/Contenido/Login/login.tsx
@@ -1,10 +1,12 @@
+import './login.css';
 
 export function LogIn(){
     return (
         <div className="login">
+            <div className="login-contenedor">
             <h1>Inicia Sesión</h1>
 
-            <form action="">
+            <form action="" className='login-formulario'>
             <div className="login-grupoinput">
                 <label htmlFor="usuario"><b>Nombre de usuario: </b></label><br />
                 <input type="text" name="usuario" id="usuario" required />
@@ -17,6 +19,7 @@ export function LogIn(){
 
             <button type="submit" className="login-boton">Enviar</button>
             </form>
+            </div>
 
         </div>
 
