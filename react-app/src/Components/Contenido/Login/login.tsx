796f1b10281abec87ba0b8fac69bc7a19096e7b7
pitumola <marcoslgl2004@gmail.com>
2024-10-25 19:18:15 +0200

    modificado el css y algun retoque en el html de la pestaña login

--- react-app/src/Components/Contenido/Login/login.tsx
+++ react-app/src/Components/Contenido/Login/login.tsx
@@ -1,31 +1,35 @@
-import './login.css';
+import "./login.css";
 
-export function LogIn(){
+export function LogIn() {
+  return (
+    <div className="login">
+      <b className="login-titulo">INICIAR SESIÓN</b>
+      <div className="login-contenedor">
+        <form action="" className="login-formulario">
+          <div className="login-grupoinput">
+            <label htmlFor="usuario">
+              <p>Nombre de usuario: </p>
+            </label>
+            <br />
+            <input type="text" name="usuario" id="usuario" required />
+          </div>
 
-    return (
-        <div className="login">
-            <div className="login-contenedor">
-            <h1>Inicia Sesión</h1>
+          <div className="login-grupoinput">
+            <label htmlFor="contrasena">
+              <p>Contraseña: </p>
+            </label>
+            <br />
+            <input type="password" name="contrasena" id="contrasena" required />
+          </div>
 
-            <form action="" className='login-formulario'>
-            <div className="login-grupoinput">
-                <label htmlFor="usuario"><b>Nombre de usuario: </b></label><br />
-                <input type="text" name="usuario" id="usuario" required />
-            </div>
-
-            <div className="login-grupoinput">
-                <label htmlFor="contrasena"><b>Contraseña: </b></label><br />
-                <input type="password" name="contrasena" id="contrasena" required />
-            </div>
-
-            <button type="submit" className="login-boton">Enviar</button>
-            <p className="login-textoRegistro">¿No tienes cuenta? <a href="">Registrate aqui</a></p>
-            </form>
-            </div>
-
-        </div>
-
-    )
-
+          <button type="submit" className="login-boton">
+            Enviar
+          </button>
+          <p className="login-textoRegistro">
+            ¿No tienes cuenta? <a href="">Registrate aqui</a>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 }
-
