d33c65cefa8cf29fa9ceddbb858cfbc236847372
edumoro412 <edumoro2003@gmail.com>
2024-10-27 16:28:23 +0100

    Nos aseguramos que el formato del correo sea el adecuado si no lo es le lanzamos una alerta

--- react-app/src/Components/Contenido/Registro/registro.tsx
+++ react-app/src/Components/Contenido/Registro/registro.tsx
@@ -1,29 +1,38 @@
 import "./Registro.css";
+import { useState } from "react";
 
 export default function Registro() {
+  const [correo, setCorreo] = useState("");
 
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+
+    const Patronemail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; //Que no empiece por @ que despues del @tenga texto y un . y despues del . tenga texto
+
+    if (!Patronemail.test(correo)) {
+      alert("Por favor, introduce un correo electrónico válido.");
+      return;
+    }
+
+    alert("Formulario enviado correctamente.");
+  };
 
   return (
-
     <div className="registro">
-
       <b className="registro-titulo">CREA TU CUENTA</b>
-
       <div className="registro-contenedor">
-
-        <form action="" className="registro-formulario">
-
+        <form onSubmit={handleSubmit} className="registro-formulario">
           <div className="registro-grupoinput">
-
             <label htmlFor="correo">
               <p>Correo electrónico: </p>
             </label>
             <br />
-            <input type="email" name="correo" id="correo" required />
+            <input type="email" name="correo"id="correo" required value={correo} onChange={(e) => setCorreo(e.target.value)} 
+            />
           </div>
 
           <div className="registro-grupoinput">
-
             <label htmlFor="usuario">
               <p>Nombre de usuario: </p>
             </label>
@@ -32,14 +41,11 @@ export default function Registro() {
           </div>
 
           <div className="registro-grupoinput">
-
-
             <label htmlFor="contrasena">
               <p>Contraseña: </p>
             </label>
             <br />
             <input type="password" name="contrasena" id="contrasena" required />
-
           </div>
 
           <button type="submit" className="registro-boton">
@@ -48,5 +54,5 @@ export default function Registro() {
         </form>
       </div>
     </div>
-  ) 
+  );
 }
